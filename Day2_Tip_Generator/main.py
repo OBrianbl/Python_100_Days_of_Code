9e8812119c804079d314c00b789116491cc2d7bd
Brandon O'Briant <obrianbl@protonmail.com>
2022-01-24 23:33:03 -0900

    added tip generator skeleton files

---
+++ Day2_Tip_Generator/main.py
@@ -0,0 +1,10 @@
+# Author: Brandon
+# Date: 2022.01.24
+# Description: This program generates tip amount given total bill amount and desired tip percentage. 
+
+# import tip generator functions
+from tip_generator_functions import *
+
+
+if __name__ == "__main__":
+    pass
